5b5533d2d138d5a5e002f57dc7fdedd5307dd59f
Samir <samirtownsley.dev@gmail.com>
2026-05-07 18:08:36 -0700

    Execution command update

--- src/execute.sh
+++ src/execute.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 #uv run python run_pipeline.py --output ../docs/ --max-trials 76000 --drugbank-csv ../drugbank_approvals.csv --all-modalities "$@"
-uv run python run_pipeline.py --output ../docs/ --max-trials 100000 --sample-seed 42 --drugbank-csv ../drugbank_approvals_smiles.csv --all-modalities --use-ct-cache --chembl-snapshot ../chembl_targets.sqlite  --opentargets-snapshot ../opentargets_snapshot.sqlite --year-range 2009-2026
+uv run python run_pipeline.py --output ../outputs/ --max-trials -1 --sample-seed 42 --drugbank-csv ../drugbank_approvals_smiles.csv --all-modalities --use-ct-cache --chembl-snapshot ../chembl_targets.sqlite  --opentargets-snapshot ../opentargets_snapshot.sqlite --year-range 2009-2026 --drop-uncached-candidates "$@"
+
